013a67ea21ece7f291acfe16727246a87f68bcac
Sam <sammybonfanti@gmail.com>
2026-02-16 19:57:56 -0700

    commit smoke tests round 2

--- tests/test_smokeTest.py
+++ tests/test_smokeTest.py
@@ -3,6 +3,7 @@ import pytest
 import time
 import json
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions
@@ -12,12 +13,14 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 class TestSmokeTest():
   def setup_method(self, method):
-    self.driver = webdriver.Firefox()
+    options = Options()
+    options.add_argument("--headless=new")
+    self.driver = webdriver.Firefox(options=options)
     self.vars = {}
-  
+
   def teardown_method(self, method):
     self.driver.quit()
-  
+
   def test_homepage(self):
     self.driver.get("http://127.0.0.1:5500/teton/1.6/index.html")
     self.driver.set_window_size(1114, 691)
@@ -26,7 +29,7 @@ class TestSmokeTest():
     assert self.driver.find_element(By.CSS_SELECTOR, ".header-title > h1").text == "Teton Idaho"
     assert self.driver.find_element(By.CSS_SELECTOR, ".header-title > h2").text == "Chamber of Commerce"
     assert self.driver.title == "Teton Idaho CoC"
-  
+
   def test_homepage2(self):
     self.driver.get("http://127.0.0.1:5500/teton/1.6/index.html")
     self.driver.set_window_size(1280, 800)
@@ -38,7 +41,7 @@ class TestSmokeTest():
     assert len(elements) > 0
     self.driver.find_element(By.LINK_TEXT, "Join Us").click()
     WebDriverWait(self.driver, 30).until(expected_conditions.presence_of_element_located((By.CSS_SELECTOR, "input[name=\"fname\"]")))
-  
+
   def test_directorypage(self):
     self.driver.get("http://127.0.0.1:5500/teton/1.6/directory.html")
     self.driver.find_element(By.ID, "directory-grid").click()
@@ -47,7 +50,7 @@ class TestSmokeTest():
     self.driver.find_element(By.ID, "directory-list").click()
     elements = self.driver.find_elements(By.XPATH, "//*[contains(text(),\'Teton Turf and Tree\')]")
     assert len(elements) > 0
-  
+
   def test_joinpage(self):
     self.driver.get("http://127.0.0.1:5500/teton/1.6/join.html")
     elements = self.driver.find_elements(By.NAME, "fname")
@@ -62,11 +65,10 @@ class TestSmokeTest():
     self.driver.find_element(By.NAME, "submit").click()
     # Wait for join-step2.html to load before checking for email field
     WebDriverWait(self.driver, 30).until(expected_conditions.presence_of_element_located((By.NAME, "email")))
-  
+
   def test_adminpage(self):
     self.driver.get("http://127.0.0.1:5500/teton/1.6/admin.html")
     elements = self.driver.find_elements(By.ID, "username")
     assert len(elements) > 0
     self.driver.find_element(By.CSS_SELECTOR, ".mysubmit").click()
     WebDriverWait(self.driver, 30).until(expected_conditions.text_to_be_present_in_element((By.CSS_SELECTOR, ".errorMessage"), "Invalid username and password."))
-  
